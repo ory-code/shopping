53505945be35b8397ba7015ca90d127d78bba666
ory-code <70527030+ory-code@users.noreply.github.com>
2022-11-17 17:41:31 +0100

    feat product , banner top and bottom the layout the cart logic and statecontext

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,29 +1,33 @@
-import Link from "next/link";
-import React from "react";
-import { urlFor } from "../lib/client";
+import React from 'react';
+import Link from 'next/link';
 
-const FooterBanner = ({ footerBanner }) => {
+import { urlFor } from '../lib/client';
+
+const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTime, smallText, midText, desc, product, buttonText, image } }) => {
   return (
     <div className="footer-banner-container">
       <div className="banner-desc">
         <div className="left">
-          <p> {footerBanner.discount} </p>
-          <h3> {footerBanner.largeText1} </h3>
-          <h3> {footerBanner.largeText2} </h3>
-          <p> {footerBanner.saletime} </p>
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
         </div>
         <div className="right">
-          <p> {footerBanner.smallText} </p>
-          <h3> {footerBanner.midText}</h3>
-          <p> {footerBanner.desc} </p>
-          <Link href={`/product/${footerBanner.product}`}>
-            <button type="type"> {footerBanner.buttonText} </button>
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
           </Link>
         </div>
-        <img src= {urlFor(footerBanner.image)}  alt={footerBanner.desc} className="footer-banner-image" />
+
+        <img 
+          src={urlFor(image)} className="footer-banner-image"
+        />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default FooterBanner;
+export default FooterBanner
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className="layout">
+      <Head>
+        <title>Sacha Shop </title>
+        <meta description="app ecommerce avec Nextjs Stripe sanity code by Sacha" />
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
